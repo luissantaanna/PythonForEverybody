2d664277f84718661a5d1be68cb06d9f9b783e08
Luis Santa Anna <luissantaanna98@hotmail.com>
2020-11-03 03:12:52 -0600

    Add files via upload

---
+++ goodvibes.py
@@ -0,0 +1,1 @@
+print("Hello my name is Luis and I am from Mexico, greetings and good vibes for all of you")
---
+++ payforwork.py
@@ -0,0 +1,5 @@
+#Pay for work
+hrs = input("Enter your hours")
+rate = input("Enter rate per hour")
+pay = float(hrs)*float(rate)
+print("Pay:", pay)
